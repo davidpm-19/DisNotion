1d53a4c934992d6e7f72b7b62138945f56f59eb4
davidpm-19 <115163224+davidpm-19@users.noreply.github.com>
2023-03-24 09:54:22 +0100

    Update DisNotion.py
    Changed some var values to generic ones and optimized the file read function

--- DisNotion.py
+++ DisNotion.py
@@ -4,16 +4,8 @@ from discord_webhook import DiscordWebhook
 
 
 def conf_import():
-    config = []
     with open('fileConf.txt', 'r') as f:
-        lines = f.readlines()
-
-    for line in lines:
-        line = line.strip()  # remove any leading or trailing whitespace
-        if line:  # check if the line is not empty
-            key, value = line.split(' : ')
-            config.append(value)
-    return config
+        return [line.split(' : ')[1] for line in f]
 
 
 def connection():
@@ -22,7 +14,7 @@ def connection():
     database_id = conf_list[1]
 
     # Discord Webhook
-    webhook_url = conf_list[2]  # Replace this with the URL of your Discord webhook
+    webhook_url = conf_list[2]
     webhook = DiscordWebhook(url=webhook_url)
     return notion, database_id, webhook
 
@@ -38,35 +30,24 @@ def discord_notion():
     for item in results:
         item_id = item["id"]
         last_edited_time = last_edited_times.get(item_id, "")
-        last_edited_status = last_edited_statuses.get(item_id, "")
         # Control to check if element has changed
         if item["last_edited_time"] != last_edited_time:
             # Variables to notify
-            entry_sprint = item["properties"]["Sprint"]['select']["name"]
-            entry_project = item["properties"]["Projects"]["title"][0]["text"]["content"]
-            entry_status = item['properties']['Status']['select']['name']
-            # Inner control to check if task has changed it status or has been completed
-            if entry_status != last_edited_status:
-                if entry_status == "Complete 🙌":
-                    message = "La tarea: " + entry_project + " del " + entry_sprint + " ha sido completada"
-                else:
-                    message = "La tarea: " + entry_project + " del " + entry_sprint + " ha cambiado de: " + last_edited_status + " a: " + entry_status
-            else:
-                message = "La tarea: " + entry_project + " del " + entry_sprint + " ha sido modificada"
+            entry_title = item["properties"]["CHANGE"]["title"][0]["text"]["content"]
+            entry_property_1 = item["properties"]["CHANGE"]['select']["name"]
+            entry_property_2 = item['properties']['CHANGE']['select']['name']
+            message = f"Change this to the message you want to send"
             # Message sender
             webhook.content = message
-            response = webhook.execute()
+            webhook.execute()
             # Update the values of dictionaries to the last ones
             last_edited_times[item_id] = item["last_edited_time"]
-            last_edited_statuses[item_id] = entry_status
-
 
 # Control dictionaries
 last_edited_times = {}
-last_edited_statuses = {}
-
 conf_list = conf_import()
 connection()
 while True:
     discord_notion()
-    time.sleep(10)
+    time.sleep(3)
+
